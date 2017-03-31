e0eac24986c06e247cf50cdcd981ea47ae42cc76
Juergen Mayer <Anskiere@Juergens-iMac.lan>
2017-03-31 20:55:18 +0200

    Add Section Slider

--- js/main.js
+++ js/main.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
     // start the Mixitup 
     var mixer = mixitup(".cons");
     
+    // start the owl carousel
+     $(".owl-carousel").owlCarousel({
+         
+         margin: 30,
+         responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        992:{
+            items:4,
+            loop:false
+        }
+    }
+     });
 });
