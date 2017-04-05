dbbeeb49fc0c3086495e5ed78ce82a8a41b13226
Juergen Mayer <Anskiere@Juergens-iMac.lan>
2017-04-05 18:40:18 +0200

    Add Scroll to top

--- js/main.js
+++ js/main.js
@@ -27,4 +27,23 @@ $(document).ready(function(){
     
     // Bring the smooth Scroll online 
     $('.nav-list li a').smoothScroll();
+    $('#myBtn').smoothScroll();
+    
+    // Add a scroll to top Btn
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+    
+    
 });
+
+
+
+    
